2e793c5b252a15af72e2fd3ffe6ef5aa22a5617f
Dakalo788 <dakalopromise@icloud.com>
2023-07-29 11:25:39 -0700

    The last digit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,3 @@
-#include <stdio.h>
-/**
- * main-Entry point
- * Discription: Prints alphabet in lowercase and then in uppercase.
- * Return: Always 0.
- */
 
-int main(void)
-{
-	char ch;
-	for (ch ='a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n);
-	return(0);
-}
 
 
